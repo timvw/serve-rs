789c94d66224741e20c485cadec6ad8d168b9fab
Tim Van Wassenhove <tim@timvw.be>
2022-04-02 18:35:02 +0200

    added very simple server with Publish method

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # serve-rs
 
 Explore messaging in Rust.
+
+```bash
+grpcurl -plaintext -import-path ./proto -proto server.proto -d '{ "message": "t"}' [::]:50051 server.Publisher/Publish
+```
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+
+pub mod server {
+    tonic::include_proto!("server"); // The string specified here must match the proto package name
+}
+
+use server::{PublishRequest, PublishResponse};
+use server::publisher_server::{PublisherServer, Publisher};
+
+#[derive(Debug, Default)]
+pub struct MyPublisher {}
+
+#[tonic::async_trait]
+impl Publisher for MyPublisher {
+    async fn publish(&self, request: Request<PublishRequest>) -> Result<Response<PublishResponse>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = PublishResponse {
+            message: format!("Hello {}!", request.into_inner().message).into(),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let publisher = MyPublisher::default();
+
+    Server::builder()
+        .add_service(PublisherServer::new(publisher))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
